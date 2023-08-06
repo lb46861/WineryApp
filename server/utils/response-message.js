b0ddd7add57a8b16bd800b78ac54fc1fe290f33b
Lovre Begovic <begovic.lovre@gmail.com>
2023-08-06 21:41:44 +0200

    Add response messages

---
+++ server/utils/response-message.js
@@ -0,0 +1,19 @@
+const formatModelName = (modelName) => {
+  let formattedModelName = modelName[0].toUpperCase() + modelName.slice(1);
+
+  // Add space before every capital letter
+  formattedModelName = formattedModelName.charAt(0) + formattedModelName.slice(1).replace(/([A-Z])/g, ' $1');
+
+  return formattedModelName;
+};
+
+const responseMessages = {
+  CREATE_SUCCESS: (modelName) => `${formatModelName(modelName)} successfully created!`,
+  UPDATE_SUCCESS: (modelName) => `${formatModelName(modelName)} successfully updated!`,
+  DELETE_SUCCESS: (modelName) => `${formatModelName(modelName)} successfully deleted!`,
+  NOT_FOUND: (modelName) => `${formatModelName(modelName)} not found!`,
+
+  INVALID_CREDENTIALS: 'Invalid username or password!'
+};
+
+module.exports = responseMessages;
